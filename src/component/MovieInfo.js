5eed428f2e3bb15c288fb20e94cff009e8b63726
Rashed Khan <abir97bd@gmail.com>
2021-01-23 20:47:34 +0600

    first commit

--- src/component/MovieInfo.js
+++ src/component/MovieInfo.js
@@ -14,28 +14,28 @@ function MovieInfo(props) {
         <Descriptions.Item label="Title" className="item">
           {movie.original_title}
         </Descriptions.Item>
-        <Descriptions.Item label="release_date" className="item">
+        <Descriptions.Item label="Release_Date" className="item">
           {movie.release_date}
         </Descriptions.Item>
-        <Descriptions.Item label="revenue" className="item">
+        <Descriptions.Item label="Revenue" className="item">
           {movie.revenue}
         </Descriptions.Item>
-        <Descriptions.Item label="runtime" className="item">
+        <Descriptions.Item label="Runtime" className="item">
           {movie.runtime}
         </Descriptions.Item>
-        <Descriptions.Item label="vote_average" span={2} className="item">
+        <Descriptions.Item label="Vote Average" span={2} className="item">
           {movie.vote_average}
         </Descriptions.Item>
-        <Descriptions.Item label="vote_count" className="item">
+        <Descriptions.Item label="Vote Count" className="item">
           {movie.vote_count}
         </Descriptions.Item>
-        <Descriptions.Item label="status" className="item">
+        <Descriptions.Item label="Status" className="item">
           {movie.status}
         </Descriptions.Item>
-        <Descriptions.Item label="popularity" className="item">
+        <Descriptions.Item label="Popularity" className="item">
           {movie.popularity}
         </Descriptions.Item>
-        <Descriptions.Item label="homepage" className="item">
+        <Descriptions.Item label="Home Page" className="item">
           <a
             href={movie.homepage}
             target="_blank"
